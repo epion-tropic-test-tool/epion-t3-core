68cac7992ab9632a1b5a381791c160c699913cb2
Nozomu Takashima <nozomu.takashima@gmail.com>
2020-09-21 22:03:59 +0900

    #1 add flow valiable.

--- src/main/java/com/epion_t3/core/common/type/FlowScopeVariables.java
+++ src/main/java/com/epion_t3/core/common/type/FlowScopeVariables.java
@@ -14,7 +14,13 @@ public enum FlowScopeVariables {
 
     CURRENT_COMMAND("currentCommand"),
 
-    CURRENT_COMMAND_EXECUTE_ID("currentProcessExecuteId"),;
+    CURRENT_COMMAND_EXECUTE_ID("currentProcessExecuteId"),
+
+    /** 実行コマンドが格納されているパス文字列. */
+    CURRENT_COMMAND_DIR("currentCommandDir"),
+
+    /** 実行コマンドが格納されているパスオブジェクト（java.nio.file.Path）. */
+    CURRENT_COMMAND_DIR_PATH("currentCommandDirPath");
 
     private String name;
 
