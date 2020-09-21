61a14d17196eeb2ee566a7bb4a8af02244792e5a
Nozomu Takashima <nozomu.takashima@gmail.com>
2020-09-21 23:05:33 +0900

    remove flow valiable...

--- src/main/java/com/epion_t3/core/common/type/FlowScopeVariables.java
+++ src/main/java/com/epion_t3/core/common/type/FlowScopeVariables.java
@@ -17,10 +17,7 @@ public enum FlowScopeVariables {
     CURRENT_COMMAND_EXECUTE_ID("currentProcessExecuteId"),
 
     /** 実行コマンドが格納されているパス文字列. */
-    CURRENT_COMMAND_DIR("currentCommandDir"),
-
-    /** 実行コマンドが格納されているパスオブジェクト（java.nio.file.Path）. */
-    CURRENT_COMMAND_DIR_PATH("currentCommandDirPath");
+    CURRENT_COMMAND_DIR("currentCommandDir");
 
     private String name;
 
