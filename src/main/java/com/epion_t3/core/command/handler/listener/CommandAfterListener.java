4cc83ffc52e06ce254d304a5a584e1ec116209ab
Nozomu Takashima <nozomu.takashima@gmail.com>
2021-02-20 17:14:15 +0900

    verup

--- src/main/java/com/epion_t3/core/command/handler/listener/CommandAfterListener.java
+++ src/main/java/com/epion_t3/core/command/handler/listener/CommandAfterListener.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2021 Nozomu Takashima. */
+/* Copyright (c) 2017-2020 Nozomu Takashima. */
 package com.epion_t3.core.command.handler.listener;
 
 import com.epion_t3.core.command.runner.CommandRunner;
--- src/main/java/com/epion_t3/core/flow/logging/factory/FlowLoggerFactory.java
+++ src/main/java/com/epion_t3/core/flow/logging/factory/FlowLoggerFactory.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2021 Nozomu Takashima. */
+/* Copyright (c) 2017-2020 Nozomu Takashima. */
 package com.epion_t3.core.flow.logging.factory;
 
 import com.epion_t3.core.flow.logging.appender.FlowLoggingAppender;
