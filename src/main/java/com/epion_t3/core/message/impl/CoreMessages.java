5bb30c564242758ed7b3d6f660683b53e2ccabd2
Nozomu Takashima <nozomu.takashima@gmail.com>
2021-05-03 18:10:50 +0900

    動作確認前コード登録.

--- src/main/java/com/epion_t3/core/message/impl/CoreMessages.java
+++ src/main/java/com/epion_t3/core/message/impl/CoreMessages.java
@@ -48,6 +48,8 @@ public enum CoreMessages implements Messages {
     CORE_ERR_0064("com.epion_t3.core.err.0064"), CORE_ERR_0065("com.epion_t3.core.err.0065"),
     CORE_ERR_0066("com.epion_t3.core.err.0066"), CORE_ERR_0067("com.epion_t3.core.err.0067"),
     CORE_ERR_0068("com.epion_t3.core.err.0068"), CORE_ERR_0069("com.epion_t3.core.err.0069"),
+    CORE_ERR_0070("com.epion_t3.core.err.0070"), CORE_ERR_0071("com.epion_t3.core.err.0071"),
+    CORE_ERR_0072("com.epion_t3.core.err.0072"), CORE_ERR_0073("com.epion_t3.core.err.0073"),
     CORE_ERR_1001("com.epion_t3.core.err.1001"), CORE_ERR_1002("com.epion_t3.core.err.1002"),
     CORE_ERR_1003("com.epion_t3.core.err.1003"), CORE_ERR_1004("com.epion_t3.core.err.1004"),
     CORE_ERR_1005("com.epion_t3.core.err.1005"), CORE_WRN_0001("com.epion_t3.core.wrn.0001"),
